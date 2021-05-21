a99d5e9e4a3804f8140c8331f9adb2efd3bd3e3a
sayanmukherjee <sayan.mukherjee@optimizeitsystems.com>
2021-05-21 20:11:30 +0530

    mongodb connection successful

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const auth_router = require('./auth/auth_route');
 const app = express();
 
+dotenv.config();
+
+mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, err => {
+    if(err){
+        console.log(err);
+    }else {
+        console.log('MongoDB connection successful');
+    }
+})
 app.use('/auth/user', auth_router);
 
 app.get('/', (req, res) => {
